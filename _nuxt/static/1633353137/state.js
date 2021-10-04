22037fecc0338142987b4e318d47909aebd4c48a
hitman00 <hitman00@users.noreply.github.com>
2021-10-04 13:12:19 +0000

    deploy: 19456474a3f52b2fb06561091113eace723b2673

---
+++ _nuxt/static/1633353137/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fsuite\u002F_nuxt\u002Fstatic\u002F1633353137",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fsuite\u002F",assetsPath:"\u002Fsuite\u002F_nuxt\u002F",cdnURL:a}}}}(null));
